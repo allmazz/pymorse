6ffdf592335c898802b131696b5faebfb230a412
luck <a@slnk.icu>
2020-02-13 18:55:56 +0000

    fix from_sound

--- pymorze.py
+++ pymorze.py
@@ -40,8 +40,8 @@ def from_morse(morse_text, alphabet):  # From morse(array) to string
 
 
 def gen_sound(morse_text, morse_filename, volume=1.0, sample=44100, dot_time=150, frequency=700):  # Generate sound file(.wav)
-    now_symble = 0  # Counter
-    morseSound = []  # Sound frames
+    now_symble = 0 # Counter
+    morse_sound = []  # Sound frames
     samp = sample / 1000
     syn = 2 * pi * frequency
     syn_pack0 = pack('h', 0) # Prepare pach for delay
@@ -56,71 +56,75 @@ def gen_sound(morse_text, morse_filename, volume=1.0, sample=44100, dot_time=150
     while now_symble < len(morse_text):  # Every character
         nowmorse_symbol = 0 # Counter
         if morse_text[now_symble] == " ":  # If space append times["space"] (delay dor space)
-            morseSound += times["space"]
+            morse_sound += times["space"]
         else:
             while nowmorse_symbol < len(morse_text[now_symble]):  # Every symbol
                 if morse_text[now_symble][nowmorse_symbol] == ".":  # If dot append times["dot"] (delay dor dot)
-                    morseSound += times["dot"]
+                    morse_sound += times["dot"]
                 elif morse_text[now_symble][nowmorse_symbol] == "-":  # If dash append times["dash"] (delay dor dash)
-                    morseSound += times["dash"]
+                    morse_sound += times["dash"]
                 nowmorse_symbol += 1
                 if nowmorse_symbol < len(morse_text[now_symble]):  # If no last morseSymbol in character append times["delay1"] (delay for morseSymbol)
-                    morseSound += times["delay1"]
+                    morse_sound += times["delay1"]
         now_symble += 1
         if now_symble < len(morse_text) and morse_text[now_symble - 1] != " " and morse_text[now_symble] != " ":  # If no last symbol in word append delay(for separate character)
-            morseSound += times["delay3"]
+            morse_sound += times["delay3"]
     morse_file = wave.open(morse_filename, "w")  # Open file
     morse_file.setparams((1, 2, sample, 0, "NONE", "not compressed"))  # Set WAV parameters
-    morse_file.writeframes(b"".join(morseSound)) # Write frames
+    morse_file.writeframes(b"".join(morse_sound)) # Write frames
     morse_file.close()  # Close file
 
 
-def read_sound(morse_filename, dot_time=150):  # Read from sound file(.wav)
+def read_sound(morse_filename, dot_time=100, dash_time=400, space_time=1000, char_space_time=400):  # Read from sound file(.wav)
     frames = []
     buf_frame = []
     w = wave.open(morse_filename, "rb")
+    frame_per_ms = w.getframerate()/1000
     # read .wav and create 0,1 array
     for i in range(0, w.getnframes()):
+
         s = w.readframes(1)
         f = int.from_bytes(s[:2], "big", signed=True)
         buf_frame.append(f)
-        if len(buf_frame) == dot_time:
+        if len(buf_frame) == 300:
             if std(buf_frame) > 5000:
                 frames.append(1)
             else:
                 frames.append(0)
             buf_frame = []
     w.close()
-
     # get time beeps
     zero_or_one = frames[0]
     num = 0
-    timeframe = []
+    time_frame = []
     out = []
     for i in range(0, len(frames)):
         if zero_or_one != frames[i]:
-            timeframe.append([num, zero_or_one])
+            time_frame.append([num*300/frame_per_ms, zero_or_one])
             zero_or_one = frames[i]
             num = 0
         else:
             num += 1
-    timeframe.append([num, zero_or_one])
+    time_frame.append([num*300/frame_per_ms, zero_or_one])
 
     # create array array with morse charts
-    print(timeframe)
+    print(time_frame)
     char = ""
-    for i in timeframe:
+    for i in time_frame:
         if i[1] == 0:
-            if i[0] > 275:
+            if i[0] > space_time:
                 out.append(char)
                 char = ""
                 out.append(" ")
-            elif i[0] > 50:
+            elif i[0] > char_space_time:
                 out.append(char)
                 char = ""
         else:
-            if i[0] > 50:
+            if i[0] > dash_time:
                 char += "-"
-            elif i[0] > 15:
+            elif i[0] > dot_time:
                 char += "."
-    return out.append(char)
+    out.append(char)
+    print(out)
+
+    return out
