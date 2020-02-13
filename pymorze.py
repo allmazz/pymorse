2d5b57c8e9351012a027fbe3538f1b79abb7f2dd
allmaZz <allmazz@DESKTOP-H8NA5GE.localdomain>
2020-02-13 04:37:42 +0300

    many fixes

--- pymorze.py
+++ pymorze.py
@@ -1,14 +1,15 @@
 # pymorze, library for Morze alphabet in Python. by allmaZz https://github.com/allmazz/
+# Thanks luck20yan https://github.com/luck20yan for help with library. Special for read .wav file(ReadSound)
 # Библиотека написанна для первого тура ЗПШ 2020
 
 import wave
-import math
-import struct
+from math import pi, sin
+from struct import pack
+from numpy import std
 
 # За русский алфавит Морзе я балогвдрен автору этой статьи: https://habr.com/ru/post/349776/
 
 # Russian Morze alphabet, author: https://habr.com/ru/post/349776/
-import numpy
 
 ruMorseAlphabet = {"А": ".-", "Б": "-...", "В": ".--", "Г": "--.", "Д": "-..", "Е": ".", "Ж": "...-", "З": "--..",
                    "И": "..", "Й": ".---", "К": "-.-", "Л": ".-..", "М": "--", "Н": "-.", "О": "---", "П": ".--.",
@@ -21,66 +22,105 @@ ruMorseAlphabet = {"А": ".-", "Б": "-...", "В": ".--", "Г": "--.", "Д": "-.
 
 
 def ToMorze(text, alphabet):  # From string to morze(array)
-    morzeText = [] # Array for symbols
+    morzeText = []  # Array for symbols
     for symbol in text.upper():
-        if symbol not in alphabet: # If symbol not in Morze dictionary
+        if symbol not in alphabet:  # If symbol not in Morze dictionary
             raise Exception("Symbol '" + symbol + "' not in Morze alphabet")
-        morzeText.append(alphabet[symbol]) # Append symbol
+        morzeText.append(alphabet[symbol])  # Append symbol
     return morzeText
 
 
 def FromMorze(morzeText, alphabet):  # From morze(array) to string
-    text = "" # String for symbols
+    text = ""  # String for symbols
     for symbol in morzeText:
         for k, v in alphabet.items():
             if v == symbol:
-                text += k # Append symbol
+                text += k  # Append symbol
     return text
 
 
-def GenSound(morzeText, morzeFilename, volume=1.0, sample=44100, time=300, frequency=700):  # Generate sound file(.wav)
-    nowSymble = 0 # Counter
-    morzeSound = [] # Sound frames
-    time3 = time * 3 # Time * 3(for delay, and long signal(dash))
-    time7 = time * 7 # Time * 7(for space)
-    while nowSymble < len(morzeText):
+def GenSound(morzeText, morzeFilename, volume=1.0, sample=44100, dotTime=150, frequency=700):  # Generate sound file(.wav)
+    nowSymble = 0  # Counter
+    morzeSound = []  # Sound frames
+    samp = sample / 1000  # Sample / 1000
+    syn = 2 * pi * frequency
+    synPack0 = pack('h', 0)
+    times = {"dot": [], "dash": [], "space": [], "delay1": [], "delay3": []}  # Dictionary for times
+    for x in range(int(dotTime * samp)):  # Calculation (dotTime * 1)(for dot)
+        times["dot"].append(pack('h', int(volume * sin(syn * (x / sample)) * 32767.0)))
+    for x in range(int(dotTime * samp)):  # Calculation delay (dotTime * 1)(for separate symbols)
+        times["delay1"].append(synPack0)
+    times["dash"] = times["dot"] * 3  # Calculate dash (dotTime * 3)
+    times["delay3"] = times["delay1"] * 3  # Calculate delay (dotTime * 3)(for separate character)
+    times["space"] = times["delay1"] * 7  # Calculate delay (dotTime * 7)(for space)
+    while nowSymble < len(morzeText):  # Every character
         nowMorzeSymbol = 0
-        if morzeText[nowSymble] == " ": # If space append time7
-            for x in range(int(time7 * (sample / 1000.0))):
-                morzeSound.append(0.0)
+        if morzeText[nowSymble] == " ":  # If space append times["space"] (delay dor space)
+            morzeSound += times["space"]
         else:
-            while nowMorzeSymbol < len(morzeText[nowSymble]):
-                print(morzeText[nowSymble][nowMorzeSymbol])
-                if morzeText[nowSymble][nowMorzeSymbol] == ".":  # If point append time
-                    for x in range(int(time * (sample / 1000.0))):
-                        morzeSound.append(volume * math.sin(2 * math.pi * frequency * (x / sample)))
-                elif morzeText[nowSymble][nowMorzeSymbol] == "-":  # If dash append time3
-                    for x in range(int(time3 * (sample / 1000.0))):
-                        morzeSound.append(volume * math.sin(2 * math.pi * frequency * (x / sample)))
+            while nowMorzeSymbol < len(morzeText[nowSymble]):  # Every symbol
+                if morzeText[nowSymble][nowMorzeSymbol] == ".":  # If dot append times["dot"] (delay dor dot)
+                    morzeSound += times["dot"]
+                elif morzeText[nowSymble][nowMorzeSymbol] == "-":  # If dash append times["dash"] (delay dor dash)
+                    morzeSound += times["dash"]
                 nowMorzeSymbol += 1
-                if nowMorzeSymbol < len(morzeText[nowSymble]): # If no last morzeSymbol in symbol append delay(time * 1)
-                    for x in range(int(time * (sample / 1000.0))):
-                        morzeSound.append(0.0)
+                if nowMorzeSymbol < len(morzeText[nowSymble]):  # If no last morzeSymbol in character append times["delay1"] (delay for morzeSymbol)
+                    morzeSound += times["delay1"]
         nowSymble += 1
-        if nowSymble < len(morzeText): # If no last symbol in word append delay(time3)
-            for x in range(int(time3 * (sample / 1000.0))):
-                morzeSound.append(0.0)
-    morzeFile = wave.open(morzeFilename, "w") # Write to file. Open file
-    morzeFile.setparams((1, 2, sample, 0, "NONE", "not compressed")) # Set WAV parameters
-    for frame in morzeSound: # Append frames
-        morzeFile.writeframes(struct.pack('h', int(frame * 32767.0)))
-    morzeFile.close() # Close file
+        if nowSymble < len(morzeText) and morzeText[nowSymble - 1] != " " and morzeText[nowSymble] != " ":  # If no last symbol in word append delay(for separate character)
+            morzeSound += times["delay3"]
+    morzeFile = wave.open(morzeFilename, "w")  # Open file
+    morzeFile.setparams((1, 2, sample, 0, "NONE", "not compressed"))  # Set WAV parameters
+    morzeFile.writeframes(b"".join(morzeSound)) # Write frames
+    morzeFile.close()  # Close file
 
 
-def ReadSound(morzeFilename, time=300): # Read from sound file(.wav)
-    w = wave.open(morzeFilename, "rb")
+def ReadSound(morzeFilename, dotTime=150):  # Read from sound file(.wav)
     frames = []
+    buf_frame = []
+    w = wave.open(morzeFilename, "rb")
+    # read .wav and create 0,1 array
     for i in range(0, w.getnframes()):
         s = w.readframes(1)
         f = int.from_bytes(s[:2], "big", signed=True)
-        if f > 10000 or i < -10000:
-            frames.append(1)
-        else:
-            frames.append(0)
+        buf_frame.append(f)
+        if len(buf_frame) == dotTime:
+            if std(buf_frame) > 5000:
+                frames.append(1)
+            else:
+                frames.append(0)
+            buf_frame = []
     w.close()
-    print(frames)
+
+    # get time beeps
+    zero_or_one = frames[0]
+    num = 0
+    timeframe = []
+    out = []
+    for i in range(0, len(frames)):
+        if zero_or_one != frames[i]:
+            timeframe.append([num, zero_or_one])
+            zero_or_one = frames[i]
+            num = 0
+        else:
+            num += 1
+    timeframe.append([num, zero_or_one])
+
+    # create array array with morse charts
+    print(timeframe)
+    char = ""
+    for i in timeframe:
+        if i[1] == 0:
+            if i[0] > 275:
+                out.append(char)
+                char = ""
+                out.append(" ")
+            elif i[0] > 50:
+                out.append(char)
+                char = ""
+        else:
+            if i[0] > 50:
+                char += "-"
+            elif i[0] > 15:
+                char += "."
+    return out.append(char)
